b46dd206c788a205538a2ee422ac3076b7ea63dc
andy820316 <andylin@Andys-MacBook-Pro.local>
2016-08-29 09:57:54 +0800

    Sample Result Page connecting to Mysql using Tomcat Servlet compiled with cordova. Signed-off-by: andy820316 <andylin@Andys-MacBook-Pro.local>

---
+++ platforms/browser/www/js/script.js
@@ -0,0 +1,76 @@
+var myList = null;
+var total_results = 0;
+var index = 0; 
+
+function storevalue(value){
+    myList = value;
+    total_results = value.length;
+}
+function getData(){
+                $.ajax({
+                type: "POST",
+                url: "http://172.20.10.12:8080/Example/Demo",
+                data: null,
+                cache: false,
+                success: function(result){
+                storevalue(JSON.parse(result));
+                }
+                });
+            }
+
+function Previous(){
+    if(index >4){
+        index = index - 5;
+    }
+    document.getElementById("box_1").innerHTML = "NAME: " + myList[index].name;
+    document.getElementById("box_2").innerHTML = "NAME: " + myList[index+1].name;
+    document.getElementById("box_3").innerHTML = "NAME: " + myList[index+2].name;
+    document.getElementById("box_4").innerHTML = "NAME: " + myList[index+3].name;
+    document.getElementById("box_5").innerHTML = "NAME: " + myList[index+4].name;
+    document.getElementById("footertext").innerHTML = "Displaying results " + (index+1) + " to " + (index+5) +" , total results: " +total_results; 
+}
+function Next(){
+    if(index+5 < total_results){
+        index = index +5;
+    }
+    document.getElementById("box_1").innerHTML = "NAME: " + myList[index].name;
+    document.getElementById("box_2").innerHTML = "NAME: " + myList[index+1].name;
+    document.getElementById("box_3").innerHTML = "NAME: " + myList[index+2].name;
+    document.getElementById("box_4").innerHTML = "NAME: " + myList[index+3].name;
+    document.getElementById("box_5").innerHTML = "NAME: " + myList[index+4].name;
+    document.getElementById("footertext").innerHTML = "Displaying results " + (index+1) + " to " + (index+5) +" , total results: " +total_results; 
+
+}
+function buildHtmlTable(selector) {
+    var columns = addAllColumnHeaders(myList, selector);
+
+    for (var i = 0 ; i < myList.length ; i++) {
+        var row$ = $('<tr/>');
+        for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+            var cellValue = myList[i][columns[colIndex]];
+
+            if (cellValue == null) { cellValue = ""; }
+
+            row$.append($('<td/>').html(cellValue));
+        }
+        $(selector).append(row$);
+    }
+};
+function addAllColumnHeaders(myList, selector)
+{
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+
+    for (var i = 0 ; i < myList.length ; i++) {
+        var rowHash = myList[i];
+        for (var key in rowHash) {
+            if ($.inArray(key, columnSet) == -1){
+                columnSet.push(key);
+                headerTr$.append($('<th/>').html(key));
+            }
+        }
+    }
+    $(selector).append(headerTr$);
+
+    return columnSet;
+}
---
+++ platforms/ios/www/bottomfoot.js
@@ -0,0 +1,18 @@
+<div name='footer-default'>© 2015 台灣電力公司 版權所有</div>
+<div name='footer-pagger' style='display:none'>
+    <span class="btn btn-primary" name="btnPrepage">
+        <i class="glyphicon glyphicon-chevron-left" data-position="left"></i>
+    </span>
+    <span class="paging-container">第&nbsp;
+        <div class="dropup btn-group">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                <span name="currpage" class="dropdown-label">0 / 0</span> <span class="caret"></span>
+            </a>
+            <ul name="pagingmenu" class="dropdown-menu scroll-menu"></ul>
+        </div>&nbsp;頁，共&nbsp;
+        <span id="rowcount">0</span>&nbsp;筆
+    </span>
+    <span class="btn btn-primary" name="btnNextpage">
+        <i class="glyphicon glyphicon-chevron-right" data-position="right"></i>
+    </span>
+</div>
