f09d7861bfff352d1ac4d32aa5eadc695c595e39
andy820316 <andylin@Andys-MacBook-Pro.local>
2016-08-30 09:37:11 +0800

    Result generated by javascript from JSON
    Result blocks now generated and deleted by javascript. Result content
    directly parsed from son file.

--- platforms/ios/www/js/script.js
+++ platforms/ios/www/js/script.js
@@ -1,6 +1,8 @@
 var myList = null;
 var total_results = 0;
 var index = 0; 
+var tempindex = 0;
+var displaycount = 0;
 
 function storevalue(value){
     myList = value;
@@ -9,40 +11,68 @@ function storevalue(value){
 function getData(){
                 $.ajax({
                 type: "POST",
-                url: "http://localhost:8080/Example/Demo",
+                url: "http://172.20.10.12:8080/Example/Demo",
                 data: null,
                 cache: false,
                 success: function(result){
                 storevalue(JSON.parse(result));
-                load();
+              //  loadresults();
+                test();
                 }
                 });
             }
 
-function load(){
-    document.getElementById("box_1").innerHTML = "NAME: " + myList[index].name;
-    document.getElementById("box_2").innerHTML = "NAME: " + myList[index+1].name;
-    document.getElementById("box_3").innerHTML = "NAME: " + myList[index+2].name;
-    document.getElementById("box_4").innerHTML = "NAME: " + myList[index+3].name;
-    document.getElementById("box_5").innerHTML = "NAME: " + myList[index+4].name;
-    document.getElementById("footertext").innerHTML = "Displaying results " + (index+1) + " to " + (index+5) +" , total results: " +total_results; 
+function nodedat(currentdocument){
+
+    for (var keys in myList[index]){
+    var text = document.createTextNode(keys + " : " + myList[index][keys]);
+    currentdocument.appendChild(text);
+    currentdocument.appendChild(document.createElement("br"));
+    };
+
+}
+
+function test(){
+    while(index < total_results && displaycount < 5){ 
+        var para = document.createElement("div");
+        para.className = "blocks";
+        var name = 'result'+index;
+        nodedat(para);
+        para.id = name;
+        var results = document.getElementById("results");
+        results.appendChild(para);
+        displaycount++;
+        index++;
+        tempindex = index-1;
+    };
+    document.getElementById("footertext").innerHTML = "Displaying results " + (index-displaycount+1) + " to " + (index) +" , total results: " +total_results; 
+}
+function remove(){
+    while(displaycount > 0){
+        console.log(tempindex);
+        var name = 'result' + tempindex;
+        var para = document.getElementById(name);
+            para.parentNode.removeChild(para);
+            displaycount--;
+            tempindex--;
+    }
 }
 
 function Previous(){
-    if(index >4){
-        index = index - 5;
+    if(index >5){
+        index = index - (5 + displaycount);
+        remove();
+        test();
     }
-   loadresults();
 }
 function Next(){
-    if(index+5 < total_results){
-        index = index +5;
+    if(index < total_results){
+        remove();
+        test();
     }
-    loadresults();
 }
 function buildHtmlTable(selector) {
     var columns = addAllColumnHeaders(myList, selector);
-ßß
     for (var i = 0 ; i < myList.length ; i++) {
         var row$ = $('<tr/>');
         for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
